07bdd352b184d2401a717409706fe709bec429aa
vagrant <vagrant@rails-dev-box>
2014-08-08 19:59:02 +0000

    Updated Gemfile for deployment to Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '6f7c7746ac78c446a295e2ea192db35a2e28cfcdaceb44e1a6a1a088d3f6b9c81899787c98cf37dacc5ae2a689dceee997ffaa767196672db53fbd2625ee17d8'
